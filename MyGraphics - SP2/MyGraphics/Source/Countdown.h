62fd20d78df9a892e22ac96b134e290187301306
CherYi <noth145@hotmail.com>
2016-02-17 16:48:11 +0800

    Countdown header

---
+++ MyGraphics - SP2/MyGraphics/Source/Countdown.h
@@ -0,0 +1,48 @@
+#include "timer.h"
+
+#ifndef COUNTDOWN_H
+#define COUNTDOWN_H
+
+class Countdown : public StopWatch
+{
+public:
+	Countdown()
+	{
+		StopWatch();
+		duration = 0;
+		timeStarted = false;
+	}
+	virtual ~Countdown()
+	{
+	}
+
+	void StartCountdown(double a)
+	{
+		timeStarted = true;
+		duration = a;
+		startTimer();
+	}
+	bool TimesUp() // true when times up
+	{
+		if (timeStarted){
+			duration -= getElapsedTime();
+
+			std::cout << duration << std::endl;
+
+			if (duration <= 0){
+				duration = 0;
+				timeStarted = false;
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+private:
+	double duration;
+	bool timeStarted;
+};
+
+#endif
